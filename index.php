412f867158a6c5eae96df77cc6a85d7bb88c4980
Patrick McGuire <insidenothing@gmail.com>
2019-01-28 15:27:49 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?PHP
+// never store passwords in code
+session_start();
+// https://support.schoology.com/hc/en-us/community/posts/115006678667-RSS-feeds
+if ($_POST['username']){
+  $_SESSION['username'] == $_POST['username'];
+  header('Location: index.php');
+}
+if ($_POST['password']){
+  $_SESSION['password'] == $_POST['password'];
+  header('Location: index.php');
+}
+?>
+<form method='POST'>
+  <input name='username' value='<?PHP echo $_SESSION['username'];?>'>
+  <input name='password' value='<?PHP echo $_SESSION['password'];?>'>
+</form>
+<hr>
