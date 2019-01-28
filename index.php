1dde2b0f7b20f8b176ab1a7a24fa1fba5d2f94f8
Patrick McGuire <insidenothing@gmail.com>
2019-01-28 15:29:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,15 @@ if ($_POST['password']){
   $_SESSION['password'] == $_POST['password'];
   header('Location: index.php');
 }
+if ($_POST['server']){
+  $_SESSION['server'] == $_POST['server'];
+  header('Location: index.php');
+}
 ?>
 <form method='POST'>
   <input name='username' value='<?PHP echo $_SESSION['username'];?>'>
   <input name='password' value='<?PHP echo $_SESSION['password'];?>'>
+  <input name='server' value='<?PHP echo $_SESSION['server'];?>'>
+  <input type='submit'>
 </form>
 <hr>
